53041332bed4ece354215181f38c2a8cffb78a0d
Bala Ganaparthi <bala.ganaparthi@okta.com>
2026-03-21 15:49:50 -0500

    Enhc : Print request

--- src/okta_mcp_server/server.py
+++ src/okta_mcp_server/server.py
@@ -26,9 +26,14 @@ class APIKeyAuthMiddleware(BaseHTTPMiddleware):
         self.exclude_paths = exclude_paths or []
 
     async def dispatch(self, request: Request, call_next):
+        
+        printRequest(request)
+        
         # Skip auth for excluded paths (e.g., /health)
         if request.url.path in self.exclude_paths:
             return await call_next(request)
+
+        
 
         # # Check x-api-key header
         # provided_key = request.headers.get("x-api-key")
@@ -48,6 +53,14 @@ class APIKeyAuthMiddleware(BaseHTTPMiddleware):
 # Global Okta client instance
 okta_client: OktaClient | None = None
 
+def printRequest(request):
+    print(request.method + ' ' + request.url + '\n---\n')
+    print('Headers : \n ' + request.headers + '\n---\n')
+    print('Body : \n' + request.body + '\n---\n')
+    print('Query Parameters : \n')
+    for key, value in request.args.items():
+        print(f"  {key}: {value}\n")
+    print('---\n')
 
 @asynccontextmanager
 async def lifespan(server: FastMCP):
